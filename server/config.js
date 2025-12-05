792d0c050d7c82766efa2460fe27c33484e45d7c
20032527 <20032527@mydbs.ie>
2025-12-05 01:24:40 +0000

    Create config.js

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  PORT: 5000,
+  JWT_SECRET_KEY: "SivaXYZ",
+  MONGO_URI: "mongodb+srv://sivanarayana018:sivanarayana018@easygadget.spral6g.mongodb.net/easygadget?retryWrites=true&w=majority"
+  };
