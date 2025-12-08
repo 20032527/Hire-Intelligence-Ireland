08c498d89649dc0564c2b01288153fe812b9a426
20032527 <20032527@mydbs.ie>
2025-12-08 20:56:03 +0000

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,68 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider, useAuth } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import GadgetForm from "./pages/GadgetForm";
+import GadgetList from "./pages/GadgetList";
+import RentalGadgetPage from "./pages/RentGadgetPage";
+import RentalHistoryPage from "./pages/RentalHistoryPage";
+
+const ProtectedRoute = ({ children }) => {
+  const { user } = useAuth();
+  return user ? children : <Login />;
+};
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+            <Route
+            path="/gadgets/create"
+            element={
+              <ProtectedRoute>
+                <GadgetForm />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/gadgets/list"
+            element={
+              <ProtectedRoute>
+                <GadgetList />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/rentals/my"
+            element={
+              <ProtectedRoute>
+                <RentalHistoryPage />
+              </ProtectedRoute>
+            }/>
+          <Route
+            path="/rentals/create"
+            element={
+              <ProtectedRoute>
+                <RentalGadgetPage />
+              </ProtectedRoute>
+            }
+          />
+
+        </Routes>
+
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
