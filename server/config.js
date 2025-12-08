a35a0da3b01f6ea9f3c3dbb8ee3cabc8bdafbbcc
20032527 <20032527@mydbs.ie>
2025-12-08 20:56:55 +0000

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: 5000,
+  PORT: 5001,
   JWT_SECRET_KEY: "SivaXYZ",
-  MONGO_URI: "mongodb+srv://sivanarayana018:sivanarayana018@easygadget.spral6g.mongodb.net/easygadget?retryWrites=true&w=majority"
-  };
+  MONGO_URI: "mongodb+srv://sivanarayana:sivanarayana@easygadget.t4b1lox.mongodb.net/easygadget?retryWrites=true&w=majority",
+};
