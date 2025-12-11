f96ceb016d510fe0b69c7522d712216758bf4c02
20032527 <20032527@mydbs.ie>
2025-12-11 23:43:07 +0000

    Create User.js

---
+++ server/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      required: true,
+    },
+    phone: {
+      type: String,
+    },
+    gadgets: {
+      model: { type: String },
+      feature: { type: String },
+      color: { type: String },
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
