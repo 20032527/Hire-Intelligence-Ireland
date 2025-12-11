0f11bbc9ecf0763df68aa41176e1684f5fc6c729
20032527 <20032527@mydbs.ie>
2025-12-11 23:42:13 +0000

    Create Rental.js

---
+++ server/models/Rental.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const RentalSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
+  gadget: { type: mongoose.Schema.Types.ObjectId, ref: 'Gadget', required: true },
+  qty: { type: Number, required: true, default: 1 },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
+  totalPrice: Number,
+  status: { type: String, enum: ['reserved','active','returned','cancelled'], default: 'reserved' }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Rental', RentalSchema);
