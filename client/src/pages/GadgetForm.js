5e127cb702f83d27c8ae44a6273e6ec53bd442e0
20032527 <20032527@mydbs.ie>
2025-12-13 17:47:28 +0000

    Create GadgetForm.js

---
+++ client/src/pages/GadgetForm.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import axios from "../api/axios";
+import "../styles/GadgetForm.css";
+
+export default function GadgetForm() {
+  const [form, setForm] = useState({
+    name: "",
+    pricePerDay: "",
+    availableQty: "",
+  });
+  const [message, setMessage] = useState("");
+
+
+  const submit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/gadgets/create", form);
+      setMessage("Gadget added successfully!");
+      setForm({ name: "", pricePerDay: "", availableQty: "" });
+    } catch (err) {
+      console.error("Error adding gadget", err);
+    }
+  };
+
+  return (
+    <div className="page-container">
+      <h2>Add Gadget</h2>
+
+      <form className="gadget-form" onSubmit={submit}>
+        <input
+          type="text"
+          placeholder="Gadget Name"
+          value={form.name}
+          onChange={(e) => setForm({ ...form, name: e.target.value })}
+        />
+
+        <input
+          type="number"
+          placeholder="Price Per Day"
+          value={form.pricePerDay}
+          onChange={(e) => setForm({ ...form, pricePerDay: e.target.value })}
+        />
+
+        <input
+          type="number"
+          placeholder="Available Quantity"
+          value={form.availableQty}
+          onChange={(e) => setForm({ ...form, availableQty: e.target.value })}
+        />
+
+        <button type="submit">Add Gadget</button>
+
+      </form>
+      {message && <p className="msg">{message}</p>}
+    </div>
+  );
+}
