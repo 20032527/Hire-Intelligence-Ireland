710821aebe1df2bfec433d7d2affa55b5c8194c6
20032527 <20032527@mydbs.ie>
2025-12-14 09:01:23 +0000

    Create gadgetController.test.js

---
+++ server/tests/gadgetController.test.js
@@ -0,0 +1,104 @@
+const Gadget = require("../models/Gadget");
+const {
+  create,
+  get,
+  update,
+  remove
+} = require("../controller/gadgetController");
+
+
+jest.mock("../models/Gadget");
+
+describe("Gadget Controller (Unit Tests)", () => {
+  
+  const mockRes = () => {
+    return {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+  };
+
+ 
+  test("create() should save gadget and return 201", async () => {
+    const req = { body: { name: "Phone", pricePerDay: 20 } };
+    const res = mockRes();
+
+    Gadget.mockImplementation(() => ({
+      save: jest.fn()
+    }));
+
+    await create(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(201);
+    expect(res.json).toHaveBeenCalled();
+  });
+
+  
+  test("get() should return gadget", async () => {
+    const req = { params: { id: "123" } };
+    const res = mockRes();
+
+    Gadget.findById.mockResolvedValue({ name: "Laptop" });
+
+    await get(req, res);
+
+    expect(res.json).toHaveBeenCalledWith({ name: "Laptop" });
+  });
+
+  test("get() should return 404 if not found", async () => {
+    const req = { params: { id: "123" } };
+    const res = mockRes();
+
+    Gadget.findById.mockResolvedValue(null);
+
+    await get(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(404);
+  });
+
+  
+  test("update() should return updated gadget", async () => {
+    const req = { params: { id: "10" }, body: { name: "Updated" } };
+    const res = mockRes();
+
+    Gadget.findByIdAndUpdate.mockResolvedValue({ name: "Updated" });
+
+    await update(req, res);
+
+    expect(res.json).toHaveBeenCalledWith({ name: "Updated" });
+  });
+
+  test("update() returns 404 for missing gadget", async () => {
+    const req = { params: { id: "10" }, body: {} };
+    const res = mockRes();
+
+    Gadget.findByIdAndUpdate.mockResolvedValue(null);
+
+    await update(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(404);
+  });
+
+  
+  test("remove() should delete gadget", async () => {
+    const req = { params: { id: "1" } };
+    const res = mockRes();
+
+    Gadget.findByIdAndDelete.mockResolvedValue({ _id: "1" });
+
+    await remove(req, res);
+
+    expect(res.json).toHaveBeenCalledWith({ message: "Deleted" });
+  });
+
+  test("remove() should return 404 when not found", async () => {
+    const req = { params: { id: "1" } };
+    const res = mockRes();
+
+    Gadget.findByIdAndDelete.mockResolvedValue(null);
+
+    await remove(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(404);
+  });
+});
